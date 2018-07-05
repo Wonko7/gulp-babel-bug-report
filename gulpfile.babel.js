b1346ff6c74756e55ee2a9322ce44f88e1259e87
William <William@undefined.re>
2018-07-05 10:37:59 +0200

    try sourceRoot fix

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,20 +1,25 @@
+const path       = require('path');
 const gulp       = require('gulp');
 const babel      = require('gulp-babel');
 const sourcemaps = require('gulp-sourcemaps');
-// const concat     = require('gulp-concat');
-const concat     = require('gulp-concat-sourcemap');
+const concat     = require('gulp-concat');
+//const concat     = require('gulp-concat-sourcemap');
+
+const writeSourceMapsOptions = {
+  sourceRoot: file => path.posix.relative(path.posix.dirname(file.relative), '')
+};
 
 gulp.task('build-concat', () =>
   gulp.src('src/**/*.js')
   .pipe(sourcemaps.init())
   .pipe(babel())
   .pipe(concat('all.js'))
-  .pipe(sourcemaps.write('.'))
+  .pipe(sourcemaps.write(writeSourceMapsOptions))
   .pipe(gulp.dest('dist-concat')));
 
 gulp.task('build', () =>
   gulp.src('src/**/*js')
   .pipe(sourcemaps.init())
   .pipe(babel())
-  .pipe(sourcemaps.write())
+  .pipe(sourcemaps.write(writeSourceMapsOptions))
   .pipe(gulp.dest('dist')));
